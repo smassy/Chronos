57a38bd29f2fe4128b224bfad79e833e58273914
S. Massy <smassy@gmail.com>
2019-04-17 12:19:46 -0400

    Add complete info for superadmin user.

--- data.sql
+++ data.sql
@@ -2,4 +2,5 @@ use chronos;
 
 -- Our super admin user
 INSERT INTO `users` VALUES (1,'superadmin','$2y$10$ffCCStgfEZJjtg.qnmn2leeMHGlwgkpiLaoeeqjCMtK5oXQFyp1CC',255);
-
+INSERT INTO departments (name) VALUES ('IT');
+INSERT INTO user_details (user_id, department_id, last_name, first_name, email, title) VALUES (1,1,'Administrator','Super','root@localhost','root');
