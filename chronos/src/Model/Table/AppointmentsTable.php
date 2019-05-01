06000f8d2ee0a4e44d994e891fb15c921f724fcf
S. Massy <smassy@gmail.com>
2019-04-30 20:38:29 -0400

    Added function to get 30-minute slot schedule with user availability.

--- chronos/src/Model/Table/AppointmentsTable.php
+++ chronos/src/Model/Table/AppointmentsTable.php
@@ -5,6 +5,8 @@ use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use \DateTime;
+use \DateInterval;
 
 /**
  * Appointments Model
@@ -45,7 +47,7 @@ class AppointmentsTable extends Table
         $this->hasOne('ExtAppointments', [
             'foreignKey' => 'appointment_id'
         ]);
-        $this->hasOne('IntAppointments', [
+        $this->hasMany('IntAppointments', [
             'foreignKey' => 'appointment_id'
         ]);
         $this->addBehavior('Calendar.Calendar', [
@@ -103,4 +105,79 @@ class AppointmentsTable extends Table
 
         return $rules;
     }
+
+    // These are used to control start and end of day
+    const DAY_START_HR = 7;
+    const DAY_START_MN = 0;
+    const DAY_END_HR = 19;
+    const DAY_END_MN = 0;
+    const SLOT_LENGTH = 'PT30M';
+
+    /**
+     * Returns an array of time slots of length SLOT_LENGTH from DAY_START to DAY_END
+     * Every slot is an element in the array with its start time length and a valued booked which is true is slot is taken for specified user on that day.
+     * @param int $user The user ID of the person whose availability is being queried.
+     * @param DateTime $day The day to check availability for.
+     */
+    public function getAvailability($user, $day = null) {
+        if (!$day) {
+            $day = new DateTime();
+        }
+        $timeSlots = [];
+        $day->setTime(self::DAY_START_HR, self::DAY_START_MN);
+        $dayEnd = clone $day;
+        $dayEnd->setTime(self::DAY_END_HR, self::DAY_END_MN);
+        $timeCursor = clone $day;
+        $step = new DateInterval('PT30M');
+        $aptStart = null;
+        $aptEnd = null;
+        $slots = 0;
+        while ($timeCursor < $dayEnd) {
+            $slotInfo = ['slot_time' => clone $timeCursor];
+            if (!$aptStart) {
+                $apt = $this->find()
+                    ->where(['start_time' => $timeCursor, 'user_id' => $user])
+                    ->first();
+                if ($apt) {
+                    if ((clone $apt->start_time)->add($step) < $apt->end_time) {
+                        $aptStart = $apt->start_time;
+                        $aptEnd = $apt->end_time;
+                        $slots = 1;
+                    } else {
+                        $slotInfo['slots'] = 1;
+                    }
+                    $slotInfo['booked'] = true;
+                    $slotInfo['start'] = $apt->start_time;
+                    $slotInfo['end'] = $apt->end_time;
+                    $slotInfo['title'] = $apt->title;
+                } else {
+                    $slotInfo['booked'] = false;
+                }
+            } else {
+                $slotInfo['booked'] = true;
+                $slotInfo['start'] = $apt->start_time;
+                $slotInfo['end'] = $apt->end_time;
+                $slotInfo['title'] = $apt->title;
+                $slots++;
+                if ((clone $timeCursor)->add($step) >= $aptEnd) {
+                    $timeSlots[count($timeSlots) + 1 - $slots]['slots'] = $slots;
+                    $slots = 0;
+                    $aptStart = null;
+                    $aptEnd = null;
+                }
+            }
+            $timeSlots[] = $slotInfo;
+            $timeCursor->add($step);
+        }
+        return $timeSlots;
+    }
+
+
+
+                    
 }
+
+
+
+
+
